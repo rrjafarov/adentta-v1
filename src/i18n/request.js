5cd81796a9c79d3f76579dbddc2e37424c61dada
rrjafarov <rafael.cefer.4050@gmail.com>
2025-05-16 17:24:47 +0400

    Refactor localization middleware to improve locale handling and static asset exclusion

--- src/i18n/request.js
+++ src/i18n/request.js
@@ -49,21 +49,84 @@
 
 
 
+// import { getRequestConfig } from "next-intl/server";
+// import { routing } from "./routing";
+// import { cookies } from "next/headers";
+
+// export default getRequestConfig(async ({ requestLocale }) => {
+//   let locale = requestLocale;
+
+//   if (!locale  || !routing.locales.includes(locale)) {
+//     locale = routing.defaultLocale;
+//   }
+
+//   const cookieStore = await cookies();
+//   const language = cookieStore.get("NEXT_LOCALE")?.value || locale;
+
+//   return {
+//     locale: language,
+//   };
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { getRequestConfig } from "next-intl/server";
+import axiosInstance from "@/api/axios";
 import { routing } from "./routing";
 import { cookies } from "next/headers";
 
 export default getRequestConfig(async ({ requestLocale }) => {
   let locale = requestLocale;
 
-  if (!locale  || !routing.locales.includes(locale)) {
+  if (!locale || !routing.locales.includes(locale)) {
     locale = routing.defaultLocale;
   }
 
   const cookieStore = await cookies();
   const language = cookieStore.get("NEXT_LOCALE")?.value || locale;
 
-  return {
-    locale: language,
-  };
+  try {
+    const response = await axiosInstance.get(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/translation-list`,
+      {
+        headers: {
+          Lang: language,
+        },
+      }
+    );
+
+    const messages = response.data || {};
+
+    return {
+      locale,
+      messages,
+    };
+  } catch (error) {
+    console.error("Failed to load translations:", error);
+    return {
+      locale,
+      messages: {},
+    };
+  }
 });
--- src/i18n/routing.js
+++ src/i18n/routing.js
@@ -1,11 +1,11 @@
+
 import { defineRouting } from "next-intl/routing";
 import { createNavigation } from "next-intl/navigation";
 
 export const routing = defineRouting({
-  locales: ["az", "ru", "en"],
+  locales: ["az", "en", "ru"],
   defaultLocale: "az",
 });
 
-// Lightweight wrappers around Next.js' navigation APIs
-// that will consider the routing configuration
-export const { Link, redirect, usePathname, useRouter } =  createNavigation(routing);
+export const { Link, redirect, usePathname, useRouter } =
+createNavigation(routing);
